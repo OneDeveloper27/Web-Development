aa92fe3f56af789751a892d52bcaa963e8393a66
OneDeveloper27 <muhammad.alfin.firdaus27@gmail.com>
2022-05-16 02:44:36 +0900

    Bautify PHP

--- Workstation 1 (BC Manokwari)/sever.php
+++ Workstation 1 (BC Manokwari)/sever.php
@@ -1,5 +1,12 @@
 <php?>
-    $host="127.0.0.1"; $port=3306; $socket=""; $user="root"; $password=""; $dbname="learning_databases"; $con = new
+    $host="127.0.0.1";
+    $port=3306; $socket="";
+    $user="root";
+    $password="Shut.Up.05";
+    $dbname="learning_databases";
+    $con = new
+
     mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' .
+
     mysqli_connect_error()); //$con->close();
     </php>
