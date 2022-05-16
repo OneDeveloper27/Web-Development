ba11eb155050154f29fcff7310a84adf734b8429
OneDeveloper27 <muhammad.alfin.firdaus27@gmail.com>
2022-05-16 11:02:22 +0900

    Editing

--- Workstation 1 (BC Manokwari)/index.php
+++ Workstation 1 (BC Manokwari)/index.php
@@ -1,12 +1,3 @@
 <?php
 mysqli_connect("localhost","root","Shut.Up.05","learning_databases")
-    $host="127.0.0.1";
-    $port=3306; $socket="";
-    $user="root";
-    $password="Shut.Up.05";
-    $dbname="learning_databases";
-    $con = new
-
-    mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' .
-    mysqli_connect_error()); //$con->close();>
 ?>
