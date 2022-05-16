f4343445289c931e299f31861a7aaa3bc86f75f5
OneDeveloper27 <muhammad.alfin.firdaus27@gmail.com>
2022-05-16 10:58:02 +0900

    Connecting Database from PHP to MySQL

--- Workstation 1 (BC Manokwari)/index.php
+++ Workstation 1 (BC Manokwari)/index.php
@@ -1,11 +1,12 @@
 <?php
+mysqli_connect(
     $host="127.0.0.1";
     $port=3306; $socket="";
     $user="root";
     $password="Shut.Up.05";
     $dbname="learning_databases";
     $con = new
-
+)
     mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' .
     mysqli_connect_error()); //$con->close();>
 ?>
