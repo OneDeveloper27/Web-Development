e88930b1cb6b1f41deaab210ec9f700239aa62cc
OneDeveloper27 <muhammad.alfin.firdaus27@gmail.com>
2022-05-16 11:00:38 +0900

    Edit mysqli connect

--- Workstation 1 (BC Manokwari)/index.php
+++ Workstation 1 (BC Manokwari)/index.php
@@ -1,12 +1,12 @@
 <?php
-mysqli_connect(
+mysqli_connect("127.0.0.1","root","Shut.Up.05","learning_databases")
     $host="127.0.0.1";
     $port=3306; $socket="";
     $user="root";
     $password="Shut.Up.05";
     $dbname="learning_databases";
     $con = new
-)
+
     mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' .
     mysqli_connect_error()); //$con->close();>
 ?>
